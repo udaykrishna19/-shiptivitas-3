5678f6f3035245105ba5402360b0ca2387e038f2
UDAY KIRAN K <136157252+udaykrishna19@users.noreply.github.com>
2024-02-03 18:32:56 +0530

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,3 +1,13 @@
+-- Query to get daily average users before and after the feature change
+SELECT DATE(time_stamp), COUNT(DISTINCT user_id)
+FROM user_activity
+WHERE DATE(time_stamp) < '2018-06-02' OR DATE(time_stamp) > '2018-06-02'
+GROUP BY DATE(time_stamp);
+
+-- Query to get the number of status changes by card
+SELECT card_id, COUNT(*)
+FROM card_status_changes
+GROUP BY card_id;
 -- TYPE YOUR SQL QUERY BELOW
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
